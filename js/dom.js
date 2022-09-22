976bf57252825fe620b92be918953a808f8e8cc2
codewithsaidul <codewithsaidul@gmail.com>
2022-09-22 20:41:29 +0600

    kkk

--- js/dom.js
+++
@@ -1,8 +0,0 @@
-const divElement = document.createAttribute('div');
-divElement.className = 'REdBil';
-divElement.setAttribute = ('id', 'redhunt');
-divElement.setAttribute = ('title', 'like ypu');
-
-const container = document.querySelector('.to-do-list');
-const h1Element = container.querySelector('h1');
-container.insertBefore(divElement, h1Element);
