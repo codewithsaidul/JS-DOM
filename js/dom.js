f98b29283f0ce9d171560593c59bad83a52d43ee
codewithsaidul <codewithsaidul@gmail.com>
2022-09-22 20:40:58 +0600

    nncc

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,8 @@
-document.title = "Play With Dom";
+const divElement = document.createAttribute('div');
+divElement.className = 'REdBil';
+divElement.setAttribute = ('id', 'redhunt');
+divElement.setAttribute = ('title', 'like ypu');
+
+const container = document.querySelector('.to-do-list');
+const h1Element = container.querySelector('h1');
+container.insertBefore(divElement, h1Element);
